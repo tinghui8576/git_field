410a2659d6f45b8b631f52032c7963b131a31514
joccker <dawson.ma10@gmail.com>
2019-09-14 16:09:08 +0800

    mend

--- vedio.py
+++ vedio.py
@@ -1,31 +1,39 @@
-import cv2
+# Python program to illustrate 
+# saving an operated video 
 
-cap = cv2.VideoCapture(0)
+# organize imports 
+import numpy as np 
+import cv2 
 
-# 設定擷取影像的尺寸大小
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+# This will return video from the first webcam on your computer. 
+cap = cv2.VideoCapture(0) 
 
-# 使用 XVID 編碼
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# Define the codec and create VideoWriter object 
+fourcc = cv2.VideoWriter_fourcc(*'XVID') 
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480)) 
 
-# 建立 VideoWriter 物件，輸出影片至 output.avi
-# FPS 值為 20.0，解析度為 640x360
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 360))
-
-while(cap.isOpened()):
-  ret, frame = cap.read()
-  if ret == True:
-    # 寫入影格
-    out.write(frame)
-
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
-  else:
+# loop runs if capturing has been initialized. 
+while(True): 
+  # reads frames from a camera 
+  # ret checks return at each frame 
+  ret, frame = cap.read() 
+  if not ret:
     break
 
-# 釋放所有資源
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+  # output the frame 
+  out.write(frame) 
+    
+  # The original input frame is shown in the window 
+  cv2.imshow('Original', frame) 
+
+  if cv2.waitKey(1) & 0xFF == ord('q'): 
+    break
+
+# Close the window / Release webcam 
+cap.release() 
+
+  # After we release our webcam, we also release the output 
+out.release() 
+
+  # De-allocate any associated memory usage 
+cv2.destroyAllWindows() 
